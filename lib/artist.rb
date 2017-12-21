68eadf3922f6c82590f1cadb60334c003a99abdb
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-21 16:08:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 class Artist
 
-  attr_accessor :name
+  attr_accessor :name, :songs
 
 def initialize(name)
   @name = name
@@ -10,10 +10,6 @@ end
 def add_song(song)
   @songs << song
   song.artist = self
-end
-
-def songs
-  self.songs
 end
 
 def genres
