626de84719a6309a3edcea17bdec8772b88a6ec3
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-21 16:04:06 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,11 +1,18 @@
 class Genre
 
+attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
     @songs = []
   end
 
+  def songs
+    @songs
+  end
 
+  def artists
+    self.songs.collect{|a| a.artist}
+  end
   
 end
